4154556b45a34e2c694f7f3790b08068a9c3860d
Aumesh Misra <aumeshmisra@wustl.edu>
2020-03-19 16:17:08 -0500

    finished building constraints and relations using pyDCOP objects

--- xmldcop.py
+++ xmldcop.py
@@ -16,6 +16,7 @@ from typing import Dict, Iterable, Union, List
 import numpy as np
 
 from xml.dom import minidom
+import yaml
 
 from pyDcop.pydcop.computations_graph import factor_graph
 from pyDcop.pydcop.dcop.dcop import DCOP
@@ -26,6 +27,13 @@ from pyDcop.pydcop.dcop.objects import (
     VariableWithCostFunc,
     VariableNoisyCostFunc,
     AgentDef,
+)
+from pydcop.dcop.relations import (
+    relation_from_str,
+    RelationProtocol,
+    NAryMatrixRelation,
+    assignment_matrix,
+    generate_assignment_as_dict,
 )
 
 
@@ -49,6 +57,7 @@ def load_dcop(filename: str) -> DCOP:
 
     dcop.domains = xml_build_domains(xmlFile)
     dcop.variables = xml_build_variables(xmlFile, dcop)
+    dcop._constraints = xml_build_constraints(xmlFile, dcop)
 
     return dcop
 
@@ -75,6 +84,53 @@ def xml_build_variables(xmlFile, dcop) -> Dict[str, Variable]:
 
     return variables_dict
 
+def xml_build_constraints(xmlFile, dcop) -> Dict[str, RelationProtocol]:
+    constraints_dict = {}
+    constraints = xmlFile.getElementsByTagName('constraint')
+
+    for constraint in constraints:
+        assignment_dict = {}
+        default = None
+        constraint_name = constraint.attributes['name'].value
+        constraint_variables = constraint.attributes['scope'].value.split(' ')
+        constraint_variables = [dcop.variable(v) for v in constraint_variables]
+        values = assignment_matrix(constraint_variables, default)
+
+        relation_name = constraint.attributes['reference'].value
+        #fix later by making a dictionary of relations
+        for relation in xmlFile.getElementsByTagName('relation'):
+            if relation.attributes['name'].value == relation_name:
+                constraintTableData = relation.childNodes[0].wholeText #string formatted values
+                constraintTableArray = constraintTableData.split('|')
+                for assignment in constraintTableArray:
+                    value_state_pair = assignment.split(':')
+                    if value_state_pair[0] not in assignment_dict:
+                        assignment_dict[value_state_pair[0]] = value_state_pair[1]
+                    else:
+                        assignment_dict[value_state_pair[0]] = str(assignment_dict[value_state_pair[0]]) + '| ' + str(value_state_pair[1])
+
+        #constructing relation protocol
+        #similar code to yamlDCOP
+        for value, assignments_def in assignment_dict.items():
+            # can be a str like "1 2 3" or "1 2 3 | 1 3 4"
+            # several assignment for the same value are separated with |
+            assignments_def = assignments_def.split("|")
+            for ass_def in assignments_def:
+                val_position = values
+                vals_def = ass_def.split()
+                for i, val_def in enumerate(vals_def[:-1]):
+                    iv, _ = constraint_variables[i].domain.to_domain_value(val_def.strip())
+                    val_position = val_position[iv]
+                # value for the last variable of the assignment
+                val_def = vals_def[-1]
+                iv, _ = constraint_variables[-1].domain.to_domain_value(val_def.strip())
+                val_position[iv] = value
+        constraints_dict[constraint_name] = NAryMatrixRelation(constraint_variables, values, name=constraint_name)
+
+    return constraints_dict
+
+
+
 def str_2_domain_values(domain_str):
     """
     Deserialize a domain expressed as a string.
@@ -99,5 +155,140 @@ def str_2_domain_values(domain_str):
         except ValueError:
             return values
 
+load_dcop('input/Rnd5-5-3.xml')
 
-load_dcop('input/Rnd3-2-1.xml')
+
+#!-----TO TEST YAML VS XML FORMAT AND OUTPUT-----!
+
+# def load_yaml_dcop(dcop_str: str) -> DCOP:
+#     loaded = yaml.load(dcop_str, Loader=yaml.FullLoader)
+#     if "name" not in loaded:
+#         raise ValueError("Missing name in dcop string")
+#     if "objective" not in loaded or loaded["objective"] not in ["min", "max"]:
+#         raise ValueError("Objective is mandatory and must be min or max")
+#
+#     dcop = DCOP(
+#         loaded["name"],
+#         loaded["objective"],
+#         loaded["description"] if "description" in loaded else "",
+#     )
+#
+#     dcop.domains = _build_domains(loaded)
+#     dcop.variables = _build_variables(loaded, dcop)
+#     dcop._constraints = _build_constraints(loaded, dcop)
+#     return dcop
+#
+# def _build_domains(loaded) -> Dict[str, VariableDomain]:
+#     domains = {}
+#     if "domains" in loaded:
+#         for d_name in loaded["domains"]:
+#             d = loaded["domains"][d_name]
+#             values = d["values"]
+#
+#             if len(values) == 1 and ".." in values[0]:
+#                 values = str_2_domain_values(d["values"][0])
+#             d_type = d["type"] if "type" in d else ""
+#             domains[d_name] = VariableDomain(d_name, d_type, values)
+#
+#     return domains
+#
+# def _build_variables(loaded, dcop) -> Dict[str, Variable]:
+#     variables = {}
+#     if "variables" in loaded:
+#         for v_name in loaded["variables"]:
+#             v = loaded["variables"][v_name]
+#             domain = dcop.domain(v["domain"])
+#             initial_value = v["initial_value"] if "initial_value" in v else None
+#             if initial_value and initial_value not in domain.values:
+#                 raise ValueError(
+#                     "initial value {} is not in the domain {} "
+#                     "of the variable {}".format(initial_value, domain.name, v_name)
+#                 )
+#
+#             if "cost_function" in v:
+#                 cost_expression = v["cost_function"]
+#                 cost_func = ExpressionFunction(cost_expression)
+#                 if "noise_level" in v:
+#                     variables[v_name] = VariableNoisyCostFunc(
+#                         v_name,
+#                         domain,
+#                         cost_func,
+#                         initial_value,
+#                         noise_level=v["noise_level"],
+#                     )
+#                 else:
+#                     variables[v_name] = VariableWithCostFunc(
+#                         v_name, domain, cost_func, initial_value
+#                     )
+#
+#             else:
+#                 variables[v_name] = Variable(v_name, domain, initial_value)
+#     return variables
+#
+# def _build_constraints(loaded, dcop) -> Dict[str, RelationProtocol]:
+#     constraints = {}
+#     if "constraints" in loaded:
+#         for c_name in loaded["constraints"]:
+#             c = loaded["constraints"][c_name]
+#             if "type" not in c:
+#                 raise ValueError(
+#                     "Error in contraints {} definition: type is "
+#                     'mandatory and only "intention" is '
+#                     "supported for now".format(c_name)
+#                 )
+#             elif c["type"] == "intention":
+#                 constraints[c_name] = relation_from_str(
+#                     c_name, c["function"], dcop.all_variables
+#                 )
+#             elif c["type"] == "extensional":
+#                 values_def = c["values"]
+#                 #print (values_def)
+#                 default = None if "default" not in c else c["default"]
+#                 if type(c["variables"]) != list:
+#                     # specific case for constraint with a single variable
+#                     v = dcop.variable(c["variables"].strip())
+#                     values = [default] * len(v.domain)
+#                     for value, assignments_def in values_def.items():
+#                         if isinstance(assignments_def, str):
+#                             for ass_def in assignments_def.split("|"):
+#                                 iv, _ = v.domain.to_domain_value(ass_def.strip())
+#                                 values[iv] = value
+#                         else:
+#                             values[v.domain.index(assignments_def)] = value
+#
+#                     constraints[c_name] = NAryMatrixRelation([v], values, name=c_name)
+#                     continue
+#
+#                 # For constraints that depends on several variables
+#                 vars = [dcop.variable(v) for v in c["variables"]]
+#                 values = assignment_matrix(vars, default)
+#                 print (values)
+#                 for value, assignments_def in values_def.items():
+#                     # can be a str like "1 2 3" or "1 2 3 | 1 3 4"
+#                     # several assignment for the same value are separated with |
+#                     assignments_def = assignments_def.split("|")
+#                     #print (value, assignments_def)
+#                     for ass_def in assignments_def:
+#                         val_position = values
+#                         vals_def = ass_def.split()
+#                         #print(vals_def)
+#                         for i, val_def in enumerate(vals_def[:-1]):
+#                             iv, _ = vars[i].domain.to_domain_value(val_def.strip())
+#                             val_position = val_position[iv]
+#                         # value for the last variable of the assignment
+#                         val_def = vals_def[-1]
+#                         iv, _ = vars[-1].domain.to_domain_value(val_def.strip())
+#                         val_position[iv] = value
+#
+#                 constraints[c_name] = NAryMatrixRelation(vars, values, name=c_name)
+#
+#             else:
+#                 raise ValueError(
+#                     "Error in contraints {} definition: type is  mandatory "
+#                     'and must be "intention" or "intensional"'.format(c_name)
+#                 )
+#
+#     return constraints
+
+#with open('input/graph_coloring_50.yaml') as file:
+    #load_yaml_dcop(file)
